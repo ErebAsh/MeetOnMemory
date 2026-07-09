21155dd996f63ba7b5529a2269bbf6ea9da8d510
SHIV RAJ SINGH <144125626+imuniqueshiv@users.noreply.github.com>
2026-07-09 09:34:32 +0530

    Change SMTP host to smtp-relay.brevo.com

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false, // Must be false for port 587
   auth: {
