17982db84fe8a1d70b20e0368836c4f5f9a1d2e0
Himanshu Raj <156138261+ErebAsh@users.noreply.github.com>
2026-07-14 14:03:58 +0530

    feat: Implement asynchronous GDPR/CCPA data export via BullMQ

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userAuth from "../middleware/userAuth.js";
-import { apiLimiter, writeLimiter } from "../middleware/rateLimiter.js";
+import { apiLimiter, writeLimiter, dataExportLimiter } from "../middleware/rateLimiter.js";
 import { requirePermission } from "../middleware/rbac.js";
 import {
   getUserData,
   updateUserProfile,
+  requestDataExport,
+  downloadExport,
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
@@ -14,5 +16,8 @@ userRouter.use(apiLimiter);
 
 userRouter.get("/data", userAuth, requirePermission("settings", "view"), getUserData);
 userRouter.put("/update", userAuth, writeLimiter, requirePermission("settings", "edit"), updateUserProfile);
+
+userRouter.post("/request-data-export", userAuth, dataExportLimiter, requirePermission("settings", "view"), requestDataExport);
+userRouter.get("/download-export/:token", downloadExport);
 
 export default userRouter;
